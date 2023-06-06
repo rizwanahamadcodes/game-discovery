296b5a3025e386e536111d6b6a7a8f019433e85f
Rizwan Ahmad <rizwanforcode@gmail.com>
2023-06-06 11:30:37 +0545

    customized the theme to get darker grays but commented it out

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,5 +1,4 @@
-import useData from "./useData";
-
+import genres from "../data/genres";
 export interface Genre {
   id: number;
   name: string;
@@ -7,6 +6,6 @@ export interface Genre {
 }
 
 const useGenres = () => {
-  return useData<Genre>("/genres");
+  return { data: genres, loading: false, error: null };
 };
 export default useGenres;
